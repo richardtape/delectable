911a5903ee0845ae85a5b84df8a5345a4cd63685
Rich Tape <richard.tape@postdesk.com>
2012-04-23 14:34:35 +0100

    Initial commit

---
+++ 0.2_0/options.js
@@ -0,0 +1,103 @@
+// Saves options to localStorage.
+function save_options(){
+  
+  	var error_msg = [];
+  
+	var e_username = document.getElementById("username");
+	var username = e_username.value;
+	if(username != ""){
+		localStorage["username"] = username;
+	}else{
+		error_msg.push("You didn't enter a username");
+	}
+	
+	var e_password = document.getElementById("password");
+	var password = e_password.value;
+	if(password != ""){
+		localStorage["password"] = password;
+	}else{
+		error_msg.push(" You didn't enter a password");
+	}
+	
+	var e_endpoint = document.getElementById("endpoint");
+	var endpoint = e_endpoint.value;
+	if(endpoint != ""){
+		localStorage["endpoint"] = endpoint;
+	}else{
+		error_msg.push(" You didn't enter an XML-RPC endpoint");
+	}
+	
+	var e_category_name = document.getElementById("category_name");
+	var category_name = e_category_name.value;
+	localStorage["category_name"] = category_name;
+	
+	var e_post_type = document.getElementById("post_type");
+	var post_type = e_post_type.value;
+	localStorage["post_type"] = post_type;
+	
+	var status = document.getElementById("status");
+	
+	if(error_msg.length > 0){
+		$('#status').fadeIn();
+		status.innerHTML = "Oops! <br />" + error_msg.toString();
+	}else{
+		$('#status').addClass('success_message').fadeIn();
+		status.innerHTML = "Options Saved.";
+		setTimeout(function() {
+			$('#status').fadeOut('slow',function(){
+				$('#status').removeClass('success_message')
+			});
+		}, 4000);
+	}
+
+}
+
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+	
+	var username = localStorage["username"];
+	var password = localStorage["password"];
+	var endpoint = localStorage["endpoint"];
+	var category_name = localStorage["category_name"];
+	var post_type = localStorage["post_type"];
+	
+	var e_username = document.getElementById("username");
+	var e_password = document.getElementById("password");
+	var e_endpoint = document.getElementById("endpoint");
+	var e_category_name = document.getElementById("category_name");
+	var e_post_type = document.getElementById("post_type");
+	
+	if(username){
+		e_username.value = username;
+	}
+	
+	if(password){
+		e_password.value = password;
+	}
+	
+	if(endpoint){
+		e_endpoint.value = endpoint;
+	}
+	
+	if(category_name){
+		e_category_name.value = category_name;
+	}
+	
+	if(post_type){
+		e_post_type.value = post_type;
+	}
+	
+}
+$(function() {
+
+	//View Advanced Options
+	$('#advanced_options').slideUp();
+	
+	$('#advanced_options_link a').click(function(){
+	
+		$('#advanced_options').slideToggle();
+		return false;
+	
+	});
+
+});
---
+++ 0.2_0/popup.js
@@ -0,0 +1,134 @@
+$(function() {
+	
+	$('#go_add_link').click(function(){
+	
+		//Add a spinner, to suggest something is happening
+		$('#working').show().html("<img src='loading.gif' alt='' />");
+		
+		//We first need to get the URL of the page we're on
+		chrome.tabs.getSelected(null,function(tab) {
+		    
+		    var tablink = tab.url;
+		    
+		   	EW.LogSystem.init(); //you can open the console by pressing ALT + D (in Firefox > 2.0 press SHIFT+ALT+D) 
+				
+			function AddBlogsListener(){            
+			}
+	
+			AddBlogsListener.prototype.connRequestError = function(errorMsg){
+			    EW.LogSystem.error("AddBlogsListener.connRequestError");
+			}
+	
+			AddBlogsListener.prototype.connRequestStopped = function(){
+			    EW.LogSystem.debug("AddBlogsListener.connRequestStopped");
+			}
+	
+			AddBlogsListener.prototype.connRequestCompleted = function(userBlogs){
+			    EW.LogSystem.debug("AddBlogsListener.connRequestCompleted");
+			}
+	
+		    try {
+		    	var username = localStorage["username"];
+				var password = localStorage["password"];
+				var endpoint = localStorage["endpoint"];
+				var category_name = localStorage["category_name"];
+				var categories_array = [];
+				var e_post_type = localStorage["post_type"];
+				
+				var set_schedule_date_time = document.getElementById("schedule_date_time");
+				var e_schedule_date_time = set_schedule_date_time.value;
+				
+				if(e_schedule_date_time != ""){
+					var schedule_date = new Date(e_schedule_date_time);
+				}
+				
+				var set_title = document.getElementById("post_title");
+				var e_post_title = set_title.value;
+				
+				if(e_post_title == ""){
+					var post_title = tablink;
+				}else{
+					var post_title = e_post_title;
+				}
+				
+				if(e_post_type !== ""){
+					set_post_type = e_post_type;
+				}else{
+					set_post_type = "post";
+				}
+				
+				var tags_array = [];
+				var set_tags = document.getElementById("post_tags");
+				var e_set_tags = set_tags.value;
+				
+				if(e_set_tags != ""){
+					
+					split_the_tags_pre = e_set_tags.replace(/^\s+|\s+$/g, '');
+					split_the_tags = split_the_tags_pre.split(",");
+					
+					for(var i in split_the_tags){
+						tags_array.push(split_the_tags[i]);
+					}
+				}
+				
+				var content_for_post = "<a href='" + tablink + "' title=''>" + tablink + "</a>";
+				
+				/*
+					For custom fields:
+					$content['custom_fields'] = array(array("key" => "import_id", "value" => $charid));
+				*/
+				
+				if(category_name && (category_name != "")){
+					
+					//A category name has been entered, so use that
+					categories_array.push(category_name);
+					
+					//Scheduled dates
+					if(schedule_date){
+					
+						var content = { title: post_title, description: content_for_post, categories: categories_array, mt_keywords: tags_array, post_type: set_post_type, dateCreated: schedule_date };
+					
+					}else{
+					
+						var content = { title: post_title, description: content_for_post, categories: categories_array, mt_keywords: tags_array, post_type: set_post_type };
+					
+					}
+					
+				}else{
+				
+					//No category name selected, so try and use the post format 'link'
+					
+					//Scheduled dates
+					if(schedule_date){
+					
+						var content = { title: post_title, description: content_for_post, wp_post_format: "link", mt_keywords: tags_array, post_type: set_post_type, dateCreated: schedule_date };
+					
+					}else{
+					
+						var content = { title: post_title, description: content_for_post, wp_post_format: "link", mt_keywords: tags_array, post_type: set_post_type };
+					
+					}
+					
+				}
+				
+				var edit_url_pre = endpoint.replace("xmlrpc.php","");
+				var edit_url = edit_url_pre + "wp-admin/edit.php";
+				
+		        var connection = new NewPost(username , password, endpoint, content);
+		        connection.addListener(new AddBlogsListener());
+		        connection.startConn();
+		        
+		        $('#working').html("<p id='complete'>Awesome! That's been <a target='_new' href='" + edit_url + "' title='Published!'>published</a>!</p>");
+		        		        
+		    } 
+		    catch (error_obj) {
+				 //EW.LogSystem.error("showErrorDialog: "+error_obj.name + "--" + error_obj.message);
+				 //alert("showErrorDialog: "+error_obj.name + "--" + error_obj.message);
+				 $('#container').append("<p>"+error_obj.name + "--" + error_obj.message+"</p>");	
+		    }
+		    
+		});
+	
+	});
+	
+});
